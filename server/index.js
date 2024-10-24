d8d44b78c20de123e8576aa986e3b1aae036c0af
Carley505 <callistusngeywa@gmail.com>
2024-10-24 17:18:31 +0300

    created a middleware and function to handle possible errors

--- server/index.js
+++ server/index.js
@@ -27,3 +27,15 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}...`)
 })
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error.'
+
+    return res.status(statusCode)
+    .json({
+        statusCode,
+        message,
+    });
+    next()
+})
