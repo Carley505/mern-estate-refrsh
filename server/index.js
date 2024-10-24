3498eebd339287154d932d92404cd35b094a1c2d
Carley505 <callistusngeywa@gmail.com>
2024-10-24 14:40:15 +0300

    create a test Api route.

--- server/index.js
+++ server/index.js
@@ -3,19 +3,20 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+
+import userRouter from './routes/user.route.js'
+
 dotenv.config();
 
 const app = express()
 const PORT = 3000
 
+app.use('/api/user', userRouter);
+
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Connection to Database Successful...")
 }).catch((err)=>{
     console.log(err.message);
-})
-
-app.get('/', (req, res)=>{
-    res.send('Hello My Guy!')
 })
 
 app.listen(PORT, ()=>{
