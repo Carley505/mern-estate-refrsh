ff4662dba187ec2db51e391c4c69875d48ad5e67
Carley505 <callistusngeywa@gmail.com>
2024-10-24 22:24:07 +0300

    complete signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,18 @@
-
-
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+      <div className="max-w-lg mx-auto">
+        <h2 className="text-center font-semibold text-3xl my-7">Sign Up</h2>
+        <form className="flex flex-col gap-4">
+          <input className="p-3 focus:outline-none border rounded-lg" type="text" id="username" placeholder="username"/>
+          <input className="p-3 focus:outline-none border rounded-lg" type="text" id="email" placeholder="email"/>
+          <input className="p-3 focus:outline-none border rounded-lg" type="password" id="password" placeholder="password"/>
+          <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">SIGN UP</button>
+          <button className="bg-red-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">CONTINUE WITH GOOGLE</button>
+        </form>
+        <p className="mt-4">Have an account? <span className="text-sky-700"><Link to="/sign-in">Sign in</Link></span></p>
+      </div>
+  );
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,7 @@
 
 
 import express from "express"
-import { test } from "../controller/user.controller.js"
+import { test } from "../controllers/user.controller.js"
 
 const router = express.Router()
 
