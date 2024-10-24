696c83c494be8f996e48ca7f6b4d579f483509b1
Carley505 <callistusngeywa@gmail.com>
2024-10-24 10:45:39 +0300

    created mongodb database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 
 
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express()
 const PORT = 3000
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connection to Database Successful...")
+}).catch((err)=>{
+    console.log(err.message);
+})
 
 app.get('/', (req, res)=>{
     res.send('Hello My Guy!')
