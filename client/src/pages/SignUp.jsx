c1b8a546afe507be72118ea1149cc2030b616bbb
Carley505 <callistusngeywa@gmail.com>
2024-10-25 12:31:27 +0300

    signup page remake by Carley505

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,11 @@
 
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export const SignUp = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
@@ -17,56 +17,48 @@ export const SignUp = () => {
   }
 
   const handleSubmit = async(e) =>{
-    e.preventDefault();
+    e.preventDefault()
 
-    setLoading(true);
+    setLoading(true)
     setError(null)
+    const url = "/api/auth/signup"
 
-    const url = "/api/auth/signup";
     await fetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers:{ "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     }).then((response)=>{
+      console.log("Response: ", response)
       return response.json()
     }).then((data)=>{
       if(data.success === false){
-        setError(data.message)
         setLoading(false)
-        console.log("FAILED: ", data)
+        setError(data.message)
+        console.log("FAILED: ", data.message)
       }else{
         setLoading(false)
-        setFormData({
-          usename: '',
-          email: '',
-          password: ''
-        });
-        navigate('/sign-in')
-        console.log("SUCCESS: ",data)
+        navigate("/sign-in")
+        console.log("SUCCESS: ", data.message)
       }
     }).catch((error)=>{
-      setError(error.message)
       setLoading(false)
-      console.log("ERROR: ", error)
-    });
+      setError(error.message)
+      console.log(error.message)
+    })
   }
 
   return (
-      <div className="max-w-lg mx-auto">
-        <h2 className="text-center font-semibold text-3xl my-7">Sign Up</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <input className="p-3 focus:outline-none border rounded-lg" type="text" id="username" placeholder="username" onChange={handleChange} value={formData.username || ''}/>
-          <input className="p-3 focus:outline-none border rounded-lg" type="text" id="email" placeholder="email" onChange={handleChange} value={formData.email || ''}/>
-          <input className="p-3 focus:outline-none border rounded-lg" type="password" id="password" placeholder="password" onChange={handleChange} value={formData.password || ''}/>
-          <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80" type="submit">
-            { loading ? "LOADING..." : "SIGN UP" }
-          </button>
-        </form>
-        <button className="bg-red-700 w-full mt-4 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">CONTINUE WITH GOOGLE</button>
-        <p className="mt-4">Have an account? <span className="text-sky-700"><Link to="/sign-in">Sign in</Link></span></p>
-        { error && <p className="text-red-600 mt-4">{ error }</p> }
-      </div>
-  );
-};
+    <div className='p-4 max-w-lg mx-auto'>
+      <h2 className='text-center font-semibold text-3xl my-7'>Sign Up</h2>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input className='p-4 border rounded-lg focus:outline-none' id='username' placeholder='username' type='text' onChange={handleChange} value={formData.username || ''} />
+        <input className='p-4 border rounded-lg focus:outline-none' id='email' placeholder='email' type='text' onChange={handleChange} value={formData.email || ''} />
+        <input className='p-4 border rounded-lg focus:outline-none' id='password' placeholder='password' type='password' onChange={handleChange} value={formData.password || ''} />
+        <button disabled={loading} className='bg-slate-700 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+      </form>
+        <button className='bg-red-700 w-full mt-4 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>continue with google</button>
+      <p className='mt-4'>Have an account? <span className='text-sky-600'><Link to="/sign-in">Sign in</Link></span></p>
+      { error && <p className='text-red-600 mt-4'>{error}</p> }
+    </div>
+  )
+}
